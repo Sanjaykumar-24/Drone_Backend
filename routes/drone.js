76392507541dfd923a1fee6f13cd5107103f71d8
Sanjaykumar <sanjaykumar24official@gmail.com>
2024-03-10 18:33:20 +0530

    addeddb

--- routes/drone.js
+++ routes/drone.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const loginModel = require('../schemas/logindb')
-
-
-
 router.post('/login', async(req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
-        res.json({ message: "all feilds required" })
+        return res.json({ message: "all feilds required" })
     }
     const user = await loginModel.findOne({username:username});
     if(!user)
     {
-        res.json({message:"failed",error:"admin not found"});
+        return res.json({message:"failed",error:"admin not found"});
     }
-    if(user.password !== password)
+    if(user.password != password)
     {
-        res.json({message:"failed",error:"wrong password"});
+        return res.json({message:"failed",error:"wrong password"});
     }
-    res.status(200).json({message:"login successfull"});
+    return res.status(200).json({message:"login successfull"});
 })
+module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const droneRoute = require('./routes/drone')
 require('dotenv').config()
 const server = express()
 server.use(express.json())
 const port = process.env.PORT || 2000
 server.use(express.urlencoded({urlencoded:false}))
 server.use(cors({origin:'*'}))
-
+server.use('/drone',droneRoute)
+mongoose.connect(process.env.URI).then(()=>{
+    console.log('Database connected sucessfully..!!🙂')
+}).catch((error)=>{
+    console.error('Datebase connection error..!!😒')
+})
+server.listen(port,()=>{
+    console.log('server started at port',port)
+})
