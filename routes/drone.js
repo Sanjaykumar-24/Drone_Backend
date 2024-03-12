16685c5f82f7e3878892aef6d0357f0d569a8755
SHESHADRI <sdri9518@gmail.com>
2024-03-12 13:01:31 +0530

    download added

--- routes/drone.js
+++ routes/drone.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const jwt = require('jsonwebtoken')
 const moment = require('moment-timezone')
 const ExcelJS = require('exceljs');
+const path = require('path')
+const fs = require('fs')
 require('dotenv').config()
 const loginModel = require('../schemas/logindb');
 const droneModel = require('../schemas/dronedb');
@@ -11,52 +13,46 @@ router.post('/login', async(req, res) => {
     if (!username || !password) {
         return res.json({ message: "all feilds required" })
     }
-    const user = await loginModel.findOne({username:username});
-    if(!user)
-    {
-        return res.json({message:"failed",error:"admin not found"});
+    const user = await loginModel.findOne({ username: username });
+    if (!user) {
+        return res.json({ message: "failed", error: "admin not found" });
     }
-    if(user.password != password)
-    {
-        return res.json({message:"failed",error:"wrong password"});
+    if (user.password != password) {
+        return res.json({ message: "failed", error: "wrong password" });
     }
-    const token = jwt.sign({id:user.id},
-        process.env.SECRET_KEY,
-        {
-            expiresIn:'20d'
+    const token = jwt.sign({ id: user.id },
+        process.env.SECRET_KEY, {
+            expiresIn: '20d'
         })
-    return res.status(200).json({message:"login successfull",token:token});
+    return res.status(200).json({ message: "login successfull", token: token });
 })
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-const verify = async(req,res,next)=>{
-    const token = req.headers.authorization.split(" ")[1];
-    if(!token)
-    {
-        return res.json({message:'failed',error:'token not found'})
+const verify = async(req, res, next) => {
+    const token = req.headers?.authorization?.split(" ")[1];
+    if (!token) {
+        return res.json({ message: 'failed', error: 'token not found' })
     }
     jwt.verify(
         token,
         process.env.SECRET_KEY,
-        async(err,user)=>{
-            if(err)
-            {
-                return res.json({message:'failed',error:err.message})
+        async(err, user) => {
+            if (err) {
+                return res.json({ message: 'failed', error: err.message })
             }
             const data = await loginModel.findById(user.id)
-            if(!data)
-            {
-                return res.json({message:'failed',error:'invaild user'})
+            if (!data) {
+                return res.json({ message: 'failed', error: 'invaild user' })
             }
             req.userId = user.id
             next()
         }
-        )
+    )
 }
-router.post('/data',verify,async(req,res)=>{
+router.post('/data', verify, async(req, res) => {
     const data = req.body
     const userId = req.userId
     const [hours1, rawMinutes1] = data.StartTime.split(':');
@@ -68,33 +64,32 @@ router.post('/data',verify,async(req,res)=>{
     const totalMinutes1 = Hours1 * 60 + minutes1;
     const totalMinutes2 = Hours2 * 60 + minutes2;
 
-   const durationInMinutes = totalMinutes2 - totalMinutes1;
+    const durationInMinutes = totalMinutes2 - totalMinutes1;
 
-   const hours = Math.floor(durationInMinutes / 60);
-   const minutes = durationInMinutes % 60;
-    const duration = hours+'h:' + minutes+'m';
-     const user = await loginModel.findById(userId)
-     data.Duration = duration
-     data.Trainer = user.username
-     let cumulativeDuration;
-     const previousCumulativeDuration = await droneModel.find();
-     let n = previousCumulativeDuration.length;
-    if (n===0) {
+    const hours = Math.floor(durationInMinutes / 60);
+    const minutes = durationInMinutes % 60;
+    const duration = hours + 'h:' + minutes + 'm';
+    const user = await loginModel.findById(userId)
+    data.Duration = duration
+    data.Trainer = user.username
+    let cumulativeDuration;
+    const previousCumulativeDuration = await droneModel.find();
+    let n = previousCumulativeDuration.length;
+    if (n === 0) {
         cumulativeDuration = duration; // Set cumulative duration to current duration if no previous entries
     } else {
-        console.log(previousCumulativeDuration[n-1].CumDuration)
-        cumulativeDuration = addTimeStrings(previousCumulativeDuration[n-1].CumDuration, duration);
+        console.log(previousCumulativeDuration[n - 1].CumDuration)
+        cumulativeDuration = addTimeStrings(previousCumulativeDuration[n - 1].CumDuration, duration);
     }
 
     data.CumDuration = cumulativeDuration;
 
-     const info = await droneModel.create(data)
-     info.save()
-     if(!info)
-     {
-        return res.json({message:'failed',error:'failed to add data'})
-     }
-     return res.json({message:'success'})
+    const info = await droneModel.create(data)
+    info.save()
+    if (!info) {
+        return res.json({ message: 'failed', error: 'failed to add data' })
+    }
+    return res.json({ message: 'success' })
 })
 
 function addTimeStrings(time1, time2) {
@@ -111,14 +106,14 @@ function addTimeStrings(time1, time2) {
     let totalHours = parsedHours1 + parsedHours2;
     let totalMinutes = minutes1 + minutes2;
 
-   
+
     totalHours += Math.floor(totalMinutes / 60);
     totalMinutes %= 60;
 
     let finalHours = totalHours % 24;
     let finalDays = Math.floor(totalHours / 24);
 
-    
+
     let result = '';
     if (finalDays > 0) {
         result += finalDays + 'd:';
@@ -132,80 +127,107 @@ function addTimeStrings(time1, time2) {
 ///////////////////////////////////////////////////////////////////////////
 
 
-router.get('/rpaslog',verify, async (req, res) => {
+router.get('/rpaslog', verify, async(req, res) => {
     try {
-      const data = await droneModel.find().exec();
-  
-      const workbook = new ExcelJS.Workbook();
-      const worksheet = workbook.addWorksheet('RPAS LOGBOOK');
-  
-      // Set column widths
-      worksheet.columns = [
-        { header: 'Date', key: 'Date', width: 15 },
-        { header: 'Name of RPIC', key: 'Trainer', width: 20 },
-        { header: 'Place of Operation', key: 'Place', width: 25 },
-        { header: 'StartTime(UTC)', key: 'StartTime', width: 18 },
-        { header: 'EndTime(UTC)', key: 'EndTime', width: 18 },
-        { header: 'Duration(HH:MM)', key: 'Duration', width: 18 },
-        { header: 'CumDuration(HH:MM)', key: 'CumDuration', width: 20 },
-      ];
-  
-      // Add data rows
-      data.forEach(item => {
-        
-        worksheet.addRow([item.Date, item.Trainer, item.Place, item.StartTime, item.EndTime, item.Duration, item.CumDuration]);
-      });
-  
-      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-      res.setHeader('Content-Disposition', 'attachment; filename="drone_data.xlsx"');
-  
-      workbook.xlsx.write(res).then(() => {
-        res.end();
-      });
+        const data = await droneModel.find().exec();
+
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet('RPAS LOGBOOK');
+
+        // Set column widths
+        worksheet.columns = [
+            { header: 'Date', key: 'Date', width: 15 },
+            { header: 'Name of RPIC', key: 'Trainer', width: 20 },
+            { header: 'Place of Operation', key: 'Place', width: 25 },
+            { header: 'StartTime(UTC)', key: 'StartTime', width: 18 },
+            { header: 'EndTime(UTC)', key: 'EndTime', width: 18 },
+            { header: 'Duration(HH:MM)', key: 'Duration', width: 18 },
+            { header: 'CumDuration(HH:MM)', key: 'CumDuration', width: 20 },
+        ];
+
+        // Add data rows
+        data.forEach(item => {
+
+            worksheet.addRow([item.Date, item.Trainer, item.Place, item.StartTime, item.EndTime, item.Duration, item.CumDuration]);
+        });
+
+        const filePath = path.join(__dirname, '..', 'temp', 'rpaslog_drone_data.xlsx');
+
+        // Write the workbook to a temporary file
+        await workbook.xlsx.writeFile(filePath);
+
+        // Send the file as a download attachment
+        res.download(filePath, 'rpaslog_drone_data.xlsx', (err) => {
+            if (err) {
+                console.error('Error sending file:', err);
+                res.status(500).send('Error sending file');
+            }
+
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error('Error deleting temporary file:', err);
+                }
+            });
+        });
     } catch (error) {
-      console.error('Error generating Excel:', error);
-      res.status(500).send('Error generating Excel');
+        console.error('Error generating Excel:', error);
+        res.status(500).send('Error generating Excel');    
     }
-  });
-  
+});
+
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-router.get('/authlog',verify, async (req, res) => {
+router.get('/authlog',verify, async(req, res) => {
     try {
-      const data = await droneModel.find().exec();
-  
-      const workbook = new ExcelJS.Workbook();
-      const worksheet = workbook.addWorksheet('FLIGHT AUTHORIZATION LOGBOOK');
-  
-      // Set column widths
-      worksheet.columns = [
-        { header: 'Date', key: 'Date', width: 15 },
-        { header: 'UNI', key: 'UNI', width: 15 },
-        { header: 'Name of RPIC', key: 'Trainer', width: 20 },
-        { header: 'Name of Trainee', key: 'Trainee', width: 20 },
-        { header: 'Exercise', key: 'Exercise', width: 15 },
-        { header: 'Duration(HH:MM)', key: 'Duration', width: 18 },
-      ];
-  
-      // Add data rows
-      data.forEach(item => {
-        worksheet.addRow([item.Date, item.UNI, item.Trainer, item.Trainee, item.Exercise, item.Duration]);
-      });
-  
-      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-      res.setHeader('Content-Disposition', 'attachment; filename="drone_data.xlsx"');
-  
-      workbook.xlsx.write(res).then(() => {
-        res.end();
-      });
+        const data = await droneModel.find().exec();
+
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet('FLIGHT AUTHORIZATION LOGBOOK');
+
+        // Set column widths
+        worksheet.columns = [
+            { header: 'Date', key: 'Date', width: 15 },
+            { header: 'UNI', key: 'UNI', width: 15 },
+            { header: 'Name of RPIC', key: 'Trainer', width: 20 },
+            { header: 'Name of Trainee', key: 'Trainee', width: 20 },
+            { header: 'Exercise', key: 'Exercise', width: 15 },
+            { header: 'Duration(HH:MM)', key: 'Duration', width: 18 },
+        ];
+
+        // Add data rows
+        data.forEach(item => {
+            worksheet.addRow([item.Date, item.UNI, item.Trainer, item.Trainee, item.Exercise, item.Duration]);
+        });
+
+        // res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        // res.setHeader('Content-Disposition', 'attachment; filename="drone_data.xlsx"');
+
+        const filePath = path.join(__dirname, '..', 'temp', 'Auth_drone_data.xlsx');
+
+        // Write the workbook to a temporary file
+        await workbook.xlsx.writeFile(filePath);
+
+        // Send the file as a download attachment
+        res.download(filePath, 'Auth_drone_data.xlsx', (err) => {
+            if (err) {
+                console.error('Error sending file:', err);
+                res.status(500).send('Error sending file');
+            }
+
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.error('Error deleting temporary file:', err);
+                }
+            });
+        });
     } catch (error) {
-      console.error('Error generating Excel:', error);
-      res.status(500).send('Error generating Excel');
+        console.error('Error generating Excel:', error);
+        res.status(500).send('Error generating Excel');    
     }
-  });
-  
+});
+
 
 module.exports = router
